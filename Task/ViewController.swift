55cc9843d6896f2590bd2cfff51127a5c3c26f3e
Muhammad Junaid <m.junaid@appinsnap.com>
2023-09-14 11:34:16 +0500

    Initial Commit

---
+++ Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task
+//
+//  Created by Muhammad Junaid on 14/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
